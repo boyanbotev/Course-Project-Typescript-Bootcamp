e47cf0fb2957bf3475456a14b99a4388e90c8471
Boyan Botev <boyanbotev@gmail.com>
2023-04-11 08:50:14 +0100

    reels spinning proto

--- src/code/components/reelContainer.ts
+++ src/code/components/reelContainer.ts
@@ -2,12 +2,15 @@ import { Container, Texture, Assets, Sprite } from "pixi.js";
 import { GameScene } from "../scenes/gameScene";
 import { SlotSymbol } from "../common/types";
 import { Manager } from "../common/manager";
+import { Tween } from "tweedle.js";
 
 export class ReelContainer extends Container {
     private reelCount: number = 4; // get from config
     private reelLength: number = 4; // get from config
     private symbolSize: number = 165; // get from config
     private topMargin: number = 65;
+
+    private isRunning: boolean = false;
 
     private scene: GameScene;
 
@@ -31,21 +34,70 @@ export class ReelContainer extends Container {
         // fragile implementation
         for (let i = 0; i < this.reelCount; i++) {
 
+            const reel = new Reel();
+            this.addChild(reel);
+
             for (let j = 0; j < this.reelLength; j++) {
-                const reel = new Reel();
-                reel.texture = symbolsBundle[reels[i][j]];
-                reel.x = this.symbolSize * i;
-                reel.y = this.symbolSize * j + this.topMargin;
-                reel.width = this.symbolSize;
-                reel.height = this.symbolSize;
-                this.addChild(reel);
+                const symbol = new Symbol();
+                symbol.texture = symbolsBundle[reels[i][j]];
+                symbol.x = this.symbolSize * i;
+                symbol.y = this.symbolSize * j + this.topMargin;
+                symbol.width = this.symbolSize;
+                symbol.height = this.symbolSize;
+                reel.addChild(symbol);
             }
 
         }
     }
+
+    public async spin(): Promise<void> {
+        console.log("spin");
+        if (this.isRunning) {
+            return;
+        }
+        this.isRunning = true;
+
+        const reels = this.children as Reel[];
+        for (let i = 0; i < reels.length; i++) {
+            const reel = reels[i];
+            // tween reel
+            const distanceToTween = this.symbolSize * 1 * Math.floor(Math.random() * 2 + 1);
+            const tween = new Tween(reel).to({y: reel.y + distanceToTween}, 10000).start();
+        }
+    }
+
+    public update(delta: number): void {
+        if (!this.isRunning) {
+            return;
+        }
+        for (let i = 0; i < this.children.length; i++) {
+            const reel = this.children[i] as Reel;
+            // if (reel.y > this.symbolSize * this.reelLength) {
+            //     reel.y = 0;
+            // }
+            for (let j = 0; j < reel.children.length; j++) {
+                const symbol = reel.children[j] as Symbol;
+                //symbol.y = (reel.y + this.symbolSize * j + this.topMargin) % (this.symbolSize * this.reelLength);
+                symbol.y = ((reel.y + j) % this.reelLength) * this.symbolSize - this.symbolSize;
+            }
+        }
+    }
+
+    public stop(): void {
+        console.log("stop");
+        this.isRunning = false;
+    }
+
+
 }
 
-class Reel extends Sprite {
+class Reel extends Container {
+    constructor() {
+        super();
+    }
+}
+
+class Symbol extends Sprite {
     constructor() {
         super();
     }
--- src/code/scenes/gameScene.ts
+++ src/code/scenes/gameScene.ts
@@ -1,9 +1,10 @@
-import { Container, Assets, Texture } from "pixi.js";
+import { Container, Assets, Texture, Ticker } from "pixi.js";
 import { IScene } from "../common/IScene";
 import { FakeAPI } from "../backend/fakeAPI";
 import { InitResponse, Request } from "../common/types";
 import { UIContainer } from "../components/uiContainer";
 import { ReelContainer } from "../components/reelContainer";
+import { Group } from "tweedle.js";
 
 export class GameScene extends Container implements IScene {
     private bet = 5;
@@ -25,6 +26,8 @@ export class GameScene extends Container implements IScene {
 
     private async createGame(): Promise<void> {
         this.init();
+        const ticker = Ticker.shared;
+        ticker.add(this.update.bind(this));
     }
 
     private async init(): Promise<void> {
@@ -44,8 +47,11 @@ export class GameScene extends Container implements IScene {
         }
         const response = await this.api.sendRequest(request);
         console.log(response);
+        this.reels.spin();
     }
 
     public update(delta: number): void {
+        Group.shared.update();
+        this.reels.update(delta);
     }
 }
