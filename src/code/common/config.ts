b250a8b45ae4fbbda7d7443c275f611293837c66
Boyan Botev <boyanbotev@gmail.com>
2023-04-16 11:23:17 +0100

    autocalulate symbolsize based on reelLength

--- src/code/common/config.ts
+++ src/code/common/config.ts
@@ -1,13 +1,17 @@
 // TODO: json
 
+
+
 export const config = {
-    reelCount: 4, // 5
-    reelLength: 4, // 4
+    reelCount: 6, // 5
+    reelLength: 6, // 4
     reelSize: 20,
 
-    symbolSize: 165, //165
+    symbolSize: 165, // 165
     topMargin: 65,
 
     initialBalance: 100,
     bet: 5,
 };
+
+config.symbolSize = 660/config.reelLength;
