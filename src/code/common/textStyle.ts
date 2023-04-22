6b8264e08f0055671fc5681f9ffb9ed88b716bec
Boyan Botev <boyanbotev@gmail.com>
2023-04-22 20:09:23 +0100

    font tweaks

--- src/code/common/textStyle.ts
+++ src/code/common/textStyle.ts
@@ -4,15 +4,15 @@ export const textStyle = new TextStyle({
     fontFamily: "Arial",
     fontSize: 30,
     dropShadow: true,
-    dropShadowColor: "#c56d6d",
+    dropShadowColor: "#a54d4d", //"#c56d6d",
     dropShadowDistance: 0.2,
     dropShadowBlur: 1,
-    fill: 0xffff99,
+    fill: 0xffee99,
     lineJoin: "round",
     stroke: "0x662233",
     strokeThickness: 3,
     align: "center",
-    fontWeight: "900",
+    fontWeight: "bold",
     letterSpacing: 5,
 });
 
@@ -32,4 +32,5 @@ export const bigTextStyle = new TextStyle({
         0.4,
         0.6
     ],
+    fontWeight: "900",
 });
